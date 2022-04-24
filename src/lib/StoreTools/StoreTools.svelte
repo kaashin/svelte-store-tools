eac409753b4398bd9b718756de4931d5c5157108
RL <richard@kaashin.com>
2022-04-23 21:49:27 -0600

    Fix if statement for checking envMode

--- src/lib/StoreTools/StoreTools.svelte
+++ src/lib/StoreTools/StoreTools.svelte
@@ -176,7 +176,7 @@
   }
 </style>
 
-{#if $ContainerStore.isOpen && (envMode === 'development' || allowInProduction)}
+{#if $ContainerStore.isOpen && ((envMode && envMode === 'development') || allowInProduction)}
   <div class="store-tools__container" style={`height: ${resizeHandle.prevHeight}px`} transition:fade={{duration: 150}} bind:this={containerEl}>
     {#if !$$props.height}
       <div 
